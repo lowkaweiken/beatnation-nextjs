3cf2b2892a8b2ff882281c57ee7e370bed3f9b5a
lowkaweiken <low_ka_wei@hotmail.com>
2024-02-20 23:46:43 +0800

    Escape Character

--- app/components/FAQ/index.tsx
+++ app/components/FAQ/index.tsx
@@ -13,7 +13,7 @@ const FAQ = () => {
                         {({ open }) => (
                             <>
                                 <Disclosure.Button className="flex w-full justify-between rounded-lg px-4 py-2 text-left text-2xl font-medium">
-                                    <span>How can beatboxing benefit my child&apos;s development?</span>
+                                    <span>How can beatboxing benefit my childs development?</span>
                                     <ChevronUpIcon
                                         className={`${open ? 'rotate-180 transform' : ''
                                             } h-5 w-5 text-purple-500`}
