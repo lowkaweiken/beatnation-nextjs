3851789e2cc458649134da88e8e8922796e145b6
lowkaweiken <low_ka_wei@hotmail.com>
2024-02-20 23:42:59 +0800

    Escape Character

--- app/components/Beliefs/index.tsx
+++ app/components/Beliefs/index.tsx
@@ -20,7 +20,7 @@ const Beliefs = () => {
                 <div className="bg-build pt-12 px-10 sm:px-24 pb-52 md:pb-70 rounded-3xl">
                     <h2 className="text-lg font-normal text-blue tracking-widest mb-5 text-center sm:text-start">BRAIN BEATS</h2>
                     <h3 className="text-4xl sm:text-65xl font-bold text-black leading-snug mb-5 text-center sm:text-start"><span className="text-blue">Enhancing</span> Creativity and Cognitive Skills</h3>
-                    <h5 className="bluish pt-2 mb-5 text-center sm:text-start">Unleash Your Child\'s Potential: Beatboxing for Enhanced Creativity and Cognitive Growth</h5>
+                    <h5 className="bluish pt-2 mb-5 text-center sm:text-start">Unleash Your Child&apos;s Potential: Beatboxing for Enhanced Creativity and Cognitive Growth</h5>
                     {/* <div className="text-center sm:text-start">
                         <button className="text-xl py-5 px-14 mt-5 font-semibold text-white rounded-full bg-blue border border-blue hover:bg-hoblue">Learn more</button>
                     </div> */}
